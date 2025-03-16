7730d942bf7b56f96bab796924d1d1e8e092fc81
DaveVED <dave.w.dennis@gmail.com>
2025-03-16 00:54:55 -0500

    feat: the build is fixed.

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # typescript-go-overlay
 
 This repository is a Nix flake packaging the [typescript-go](https://github.com/microsoft/typescript-go) project. The flake tracks and builds Microsoft's TypeScript Go implementation, providing nightly builds.
+
+## Thanks
+
+Project structure was taken from [here](https://github.com/mitchellh/zig-overlay/tree/main) as a base. Mitchell Hashimotos github seems to be a knowledge bank for nix. Thanks for the learning baseline. 
--- flake.nix
+++ flake.nix
@@ -1,31 +1,30 @@
 {
-  description = "TypeScript-Go - An implementation of TypeScript in Go";
+  description = "🟪 typescript-go-overlay 🦉";
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+    
     flake-compat = {
       url = "github:edolstra/flake-compat";
       flake = false;
     };
   };
-  outputs = { self, nixpkgs, flake-utils, ... }:
-    flake-utils.lib.eachSystem ["x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin"] (system:
-      let
-        pkgs = import nixpkgs {
-          inherit system;
-        };
-        
-        sources = builtins.fromJSON (builtins.readFile ./sources.json);
-        
-        typescript-go-overlay = final: prev: {
-          typescript-go-nightly = final.callPackage ./default.nix {
-            sourceInfo = sources.typescript-go-nightly;
+  outputs = { self, nixpkgs, flake-utils, ... }: 
+    let
+      # Import sources.json
+      sources = builtins.fromJSON (builtins.readFile ./sources.json);
+      
+      # Define systems to support
+      systems = ["x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin"];
+      
+      # System-specific outputs
+      outputs = flake-utils.lib.eachSystem systems (system:
+        let
+          pkgs = import nixpkgs {
+            inherit system;
           };
-        };
-      in
-      {
-        overlays.default = typescript-go-overlay;
-        
+        in
+        {
         packages = {
           default = pkgs.callPackage ./default.nix {
             sourceInfo = sources.typescript-go-nightly;
@@ -58,6 +57,21 @@
         };
         
         formatter = pkgs.nixpkgs-fmt;
-      }
-    );
+      });
+    in
+      outputs
+      // {
+        # Define overlay at the top level, outside eachSystem
+        overlays.default = final: prev: {
+          typescript-go-nightly = final.callPackage ./default.nix {
+            sourceInfo = sources.typescript-go-nightly;
+          };
+        };
+        
+        # Templates
+        templates.typescript-go = {
+          path = ./templates/typescript-go;
+          description = "A basic TypeScript-Go development environment.";
+        };
+      };
 }
